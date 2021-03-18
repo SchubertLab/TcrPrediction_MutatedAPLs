aaaf951aee273ec7d6d9ae0676c87cffa3fa9056
e-dorigatti <emilio.dorigatti@gmail.com>
2021-03-18 11:27:20 +0100

    t-specific classification with new data

--- tcr_specific_rf_classification.py
+++ tcr_specific_rf_classification.py
@@ -1,51 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# this runs a default random forest model on B11, B15 and OT1
-#
+# this runs a tcr-specific random forest to classify whether
+# a tcr is activated following a certain mutation
 
-
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
 from sklearn.model_selection import KFold
 from tqdm import tqdm
 from sklearn.ensemble import RandomForestClassifier
-from preprocessing import get_dataset, get_aa_features, get_features
+from preprocessing import get_dataset, get_aa_features, full_aa_features
 
 
 df = get_dataset()
 df['is_activated'] = df['activation'] > 15
+data = df[(
+    df['mut_pos'] >= 0
+) & (
+    df['tcr'].isin(df.query('is_activated')['tcr'].unique())
+)]
 aa_features = get_aa_features()
+train_data = full_aa_features(data, aa_features)
 
-#%%
+#%% training
 
-def tcr_specific_model_classification(
-    use_orig_props: bool,
-    use_mutated_props: bool,
-    use_diff_props: bool,
-    use_sequence_1hot: bool,
-    use_sequence_props: bool,
-    use_sequence_diff: bool,
-    experiment_name: str,
-):
+def tcr_specific_model_classification():
     perf = []
 
-    # build training features
-    data = df[df['mut_pos'] >= 0]
-    train_data = get_features(data, aa_features, use_orig_props,
-                              use_mutated_props, use_diff_props,
-                              use_sequence_1hot, use_sequence_props,
-                              use_sequence_diff)
     print('training on', train_data.shape[1], 'features')
-
-    for t in ['B11', 'B15', 'OT1', 'F4', 'B13', 'G6']:
+    for t in tqdm(data['tcr'].unique()):
         fit_mask = (data['tcr'] == t)
         fit_data = train_data[fit_mask]
 
         split = KFold(len(fit_data), shuffle=True).split(fit_data)
-        for i, (train_idx, test_idx) in tqdm(enumerate(split), total=len(fit_data)):
+        for i, (train_idx, test_idx) in enumerate(split):
             xtrain = fit_data.iloc[train_idx]
             xtest = fit_data.iloc[test_idx]
 
@@ -68,37 +59,20 @@ def tcr_specific_model_classification(
     # aggregate performance data
     pdf = pd.concat(perf)
 
-    pdf['use_mutated_props'] = use_mutated_props
-    pdf['use_sequence_1hot'] = use_sequence_1hot
-    pdf['use_sequence_props'] = use_sequence_props
-
-    if experiment_name:
-        pdf['features'] = experiment_name
-
     return pdf
 
-#%% training
 
-pdf = tcr_specific_model_classification(
-    use_orig_props=True,
-    use_mutated_props=True,
-    use_diff_props=True,
-    use_sequence_1hot=True,
-    use_sequence_props=True,
-    use_sequence_diff=True,
-    experiment_name='full',
-)
+fname = 'results/tcr_specific_classification_performance.csv'
+if not os.path.exists(fname):
+    print('computing results for the first time')
+    pdf = tcr_specific_model_classification()
+    pdf.to_csv(fname, index=False)
+else:
+    pdf = pd.read_csv(fname)
 
-pdf.to_csv('tcr_specific_classification_performance.csv', index=False)
+#%% probability plots
 
-#%% load evaluatio data
-
-pdf = pd.read_csv('tcr_specific_classification_performance.csv')
-
-
-#%%
-
-g = sns.FacetGrid(pdf, col='tcr', col_wrap=2, ylim=(0, 1))
+g = sns.FacetGrid(pdf, col='tcr', col_wrap=9, ylim=(0, 1))
 g.map(sns.stripplot,  'is_activated', 'pred', 'mut_pos',
       order=[False, True], hue_order=range(8), palette='husl')
 g.map(sns.pointplot, 'is_activated', 'pred', color='C3', order=[False, True])
@@ -108,27 +82,59 @@ for ax in g.axes:
 g.add_legend()
 plt.savefig('figures/tcr_specific_activation_prediction.pdf', dpi=192)
 
+#%% separate roc curves
 
-#%%
+ntcrs = len(pdf['tcr'].unique())
+ncols = 8
+height = 2.5
+nrows = ntcrs // ncols + 1
 
-plt.figure(figsize=(6, 9))
+plt.figure(figsize=(ncols * height, nrows * height))
 for i, (tcr, q) in enumerate(pdf.groupby('tcr')):
-    plt.subplot(3, 2, i + 1)
+    plt.subplot(nrows, ncols, i + 1)
     fpr, tpr, thr = roc_curve(q['is_activated'], q['pred'])
     auc = roc_auc_score(q['is_activated'], q['pred'])
+
+    pr, rc, thr = precision_recall_curve(q['is_activated'], q['pred'])
+    aps = average_precision_score(q['is_activated'], q['pred'])
+
     plt.plot(fpr, tpr)
-    plt.title(f'{tcr} - {auc:.3f}')
+    plt.plot(rc, pr)
+    plt.title(f'{tcr}\nAUC: {auc:.2f} APS: {aps:.2f}')
+
     plt.xlim(-0.1, 1.1)
     plt.ylim(-0.1, 1.1)
-    if i == 0 or i == 3:
-        plt.ylabel('TPR')
+
+    ax = plt.gca()
+    if ax.is_first_col():
+        plt.ylabel('TPR / Pr.')
     else:
         plt.yticks([])
-    if i > 2:
-        plt.xlabel('FPR')
+
+    if ax.is_last_row():
+        plt.xlabel('FPR / Rec.')
     else:
         plt.xticks([])
 
 plt.tight_layout()
 sns.despine()
 plt.savefig('figures/tcr_specific_activation_aucs.pdf', dpi=192)
+
+#%% roc curves together
+
+colors = sns.color_palette("husl", len(pdf['tcr'].unique()))
+plt.figure(figsize=(5, 5))
+for i, (tcr, q) in enumerate(pdf.groupby('tcr')):
+    fpr, tpr, thr = roc_curve(q['is_activated'], q['pred'])
+    auc = roc_auc_score(q['is_activated'], q['pred'])
+    plt.plot(fpr, tpr, c=colors[i], label=tcr)
+
+plt.xlim(-0.1, 1.1)
+plt.ylim(-0.1, 1.1)
+
+plt.ylabel('TPR')
+plt.xlabel('FPR')
+
+plt.tight_layout()
+sns.despine()
+plt.savefig('figures/tcr_specific_activation_aucs_together.pdf', dpi=192)
