1ad7af42d079fd7bd6b080adab21c72f1e8e0cf5
e-dorigatti <emilio.dorigatti@gmail.com>
2021-06-28 16:19:16 +0200

    comparing full and reduced features on tcr stratified classification

--- activation-prediction/tcr_stratified_classification.py
+++ activation-prediction/tcr_stratified_classification.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from adjustText import adjust_text
+from scipy import stats
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, roc_curve
@@ -50,36 +51,39 @@ def train():
     # disable parallel processing if running from within spyder
     n_jobs = 1 if 'SPY_PYTHONPATH' in os.environ else -1
 
-    fit_data = full_aa_features(tdf, aa_features, include_tcr=True)
-    print('total features', fit_data.shape[1])
-
     perf = []
-    for _, data_mask in tqdm(masked_groupby(tdf, ['normalization', 'threshold'])):
-        for test_tcr in tqdm(tdf['tcr'].unique(), ncols=50):
-            train_mask = data_mask & (tdf['tcr'] != test_tcr)
-            test_mask = data_mask & (tdf['tcr'] == test_tcr)
-
-            if not train_mask.any() or not test_mask.any():
-                continue
-
-            xtrain = fit_data[train_mask]
-            xtest = fit_data[test_mask]
-            ytrain = tdf.loc[train_mask, 'is_activated']
-
-            # train and predict
-            reg = RandomForestClassifier(
-                n_estimators=1000,
-                n_jobs=n_jobs,
-            ).fit(xtrain, ytrain)
-            test_preds = reg.predict_proba(xtest)
-
-            # save performance
-            pdf = tdf[test_mask][[
-                'normalization', 'threshold', 'tcr', 'mut_pos', 'mut_ami',
-                'wild_activation', 'activation', 'is_activated'
-            ]]
-            pdf['pred'] = test_preds[:, 1]
-            perf.append(pdf)
+    for reduce_feats in [False, True]:
+        feats = aa_features if reduce_feats else aa_features[['factors']]
+        fit_data = full_aa_features(tdf, feats, include_tcr=True)
+        print('training on', fit_data.shape[1], 'features')
+        
+        for _, data_mask in tqdm(masked_groupby(tdf, ['normalization', 'threshold'])):
+            for test_tcr in tqdm(tdf['tcr'].unique(), ncols=50):
+                train_mask = data_mask & (tdf['tcr'] != test_tcr)
+                test_mask = data_mask & (tdf['tcr'] == test_tcr)
+    
+                if not train_mask.any() or not test_mask.any():
+                    continue
+    
+                xtrain = fit_data[train_mask]
+                xtest = fit_data[test_mask]
+                ytrain = tdf.loc[train_mask, 'is_activated']
+    
+                # train and predict
+                reg = RandomForestClassifier(
+                    n_estimators=1000,
+                    n_jobs=n_jobs,
+                ).fit(xtrain, ytrain)
+                test_preds = reg.predict_proba(xtest)
+    
+                # save performance
+                pdf = tdf[test_mask][[
+                    'normalization', 'threshold', 'tcr', 'mut_pos', 'mut_ami',
+                    'wild_activation', 'activation', 'is_activated'
+                ]]
+                pdf['pred'] = test_preds[:, 1]
+                pdf['reduced_features'] = reduce_feats
+                perf.append(pdf)
 
     ppdf = pd.concat(perf)
 
@@ -90,9 +94,8 @@ fname = 'results/tcr_stratified_classification_performance.csv.gz'
 if not os.path.exists(fname):
     pdf = train()
     pdf.to_csv(fname, index=False)
-else:
-    print('using cached results')
-    pdf = pd.read_csv(fname)
+    
+pdf = pd.read_csv(fname)
 
 # %% load evaluation data
 pdf = pdf.query('mut_pos >= 0')
@@ -103,67 +106,58 @@ pdf = pdf.groupby([
 #%% comparing normalizations
 
 pp = pdf.groupby(
-    ['normalization', 'tcr', 'threshold'], as_index=False
+    ['reduced_features', 'normalization', 'tcr', 'threshold'], as_index=False
 ).apply(lambda q: pd.Series({
     'auc': metrics.roc_auc_score(q['is_activated'], q['pred']),
     'aps': metrics.average_precision_score(q['is_activated'], q['pred']),
-})).melt(['normalization', 'tcr', 'threshold']).pivot_table(
-    'value', ['tcr', 'threshold'], ['variable', 'normalization']
+})).melt(['reduced_features', 'normalization', 'tcr', 'threshold']).pivot_table(
+    'value', ['tcr', 'threshold', 'normalization', 'reduced_features'], 'variable'
+).reset_index()
+
+pp.loc[:, 'reduced_features'] = np.where(pp['reduced_features'], 'redux', 'full')
+
+    
+#%% are reduced features better?
+
+g = sns.catplot(
+    data=pp[(
+        (pp['normalization'] == 'AS') & (pp['threshold'] == 46.9)
+    ) | (
+        (pp['normalization'] == 'OT1') & (pp['threshold'] == 66.8)
+    ) | (
+        (pp['normalization'] == 'none') & (pp['threshold'] == 15)
+    )],
+    col='normalization',
+    x='reduced_features', y='auc',
+    dodge=True, kind='box', margin_titles=True,
+    height=3, aspect=0.5,
 )
 
-adf = pp['auc'].reset_index()
-adf['is_educated'] = adf['tcr'].str.startswith('ED')
-print(adf.groupby('is_educated')['AS'].apply(lambda g: g.describe()))
+        
+def do_test(feats, auc, **kwargs):
+    x1 = auc[feats == 'redux']
+    x2 = auc[feats == 'full']
+    
+    r = stats.ttest_rel(x1, x2, alternative='greater')
+    
+    # bonferroni
+    ntests = 3
+    if r.pvalue < 0.05 / ntests:
+        sig = ' *'
+    elif r.pvalue < 0.01 / ntests:
+        sig = ' **'
+    elif r.pvalue < 0.001 / ntests:
+        sig = ' ***'
+    else:
+        sig = ''
+    
+    plt.text(0.05, 0.05, f'N = {len(x1)}\nt = {r.statistic:.3f}\np = {r.pvalue:.1e}{sig}',
+             backgroundcolor='#ffffff77',
+             transform=plt.gca().transAxes, va='bottom')
 
-#%% predicted probabilities
-g = sns.FacetGrid(
-    pdf.query('normalization=="AS" & threshold == 46.9'),
-    col='tcr', col_wrap=8, ylim=(0, 1), height=2
-)
+g.map(do_test, 'reduced_features', 'auc')
 
-g.map(sns.stripplot, 'is_activated', 'pred', 'mut_pos',
-      order=[False, True], hue_order=range(8), palette='husl')
-g.map(sns.pointplot, 'is_activated', 'pred', color='C3', order=[False, True])
-for ax in g.axes:
-    plt.setp(ax.lines, zorder=100)
-    plt.setp(ax.collections, zorder=100, label="")
-g.add_legend()
-plt.savefig('figures/tcr_stratified_activation_prediction_AS.pdf', dpi=192)
-
-#%% paired AUC comparison
-
-vs = ['AS', 'OT1', 'none']
-g = sns.pairplot(
-    data=pp['auc'].reset_index(),
-    hue='threshold', vars=vs, palette='tab10',
-)
-
-# annotate
-for i, row in enumerate(g.axes):
-    for j, ax in enumerate(row):
-        if i == j:
-            continue
-
-        ax.set_xlim(-0.1, 1.1)
-        ax.set_ylim(-0.1, 1.1)
-        ax.plot([0, 1], [0, 1], 'r--')
-
-        data_x = pp['auc'][vs[j]]
-        data_y = pp['auc'][vs[i]]
-
-        diff = (data_x - data_y).abs().dropna()
-        annot = diff > 0.15
-
-        txt = [
-            ax.text(data_x[tcr], data_y[tcr], tcr)
-            for tcr in diff[annot].index
-        ]
-
-        adjust_text(txt, arrowprops=dict(arrowstyle='-'), ax=ax)
-
-g.fig.suptitle('AUC comparison by normalization')
-g.tight_layout()
-g.fig.savefig('figures/tcr_stratified_auc_by_norm.pdf', dpi=192)
+plt.savefig('figures/tcr_stratified_feature_comparison.pdf', dpi=192)
 
 
 #%% all aucs together
@@ -171,18 +165,18 @@ g.fig.savefig('figures/tcr_stratified_auc_by_norm.pdf', dpi=192)
 pdf['is_educated'] = pdf['tcr'].str.startswith('ED')
 
 naive_colors = sns.color_palette(
-    'Oranges', n_colors=len(pdf.query('~is_educated')['tcr'].unique()) + 4
+    'Oranges', n_colors=2 * len(pdf.query('~is_educated')['tcr'].unique()) + 4
 )
 naive_idx = 1
 
 educated_colors = sns.color_palette(
-    'Blues', n_colors=len(pdf.query('is_educated')['tcr'].unique()) + 4
+    'Blues', n_colors=2 * len(pdf.query('is_educated')['tcr'].unique()) + 4
 )
 educated_idx = 1
 
 plt.figure(figsize=(4 * 1.25, 4))
-groups = pdf.query('normalization == "AS" & threshold == 46.9').groupby('tcr')
-for i, (tcr, g) in enumerate(groups):
+groups = pdf.query('reduced_features & normalization == "AS" & threshold == 46.9').groupby(['reduced_features', 'tcr'])
+for i, ((fs, tcr), g) in enumerate(groups):
     fpr, tpr, _ = metrics.roc_curve(g['is_activated'], g['pred'])
     auc = metrics.roc_auc_score(g['is_activated'], g['pred'])
 
@@ -222,7 +216,7 @@ nrows = ntcrs // ncols + 1
 cm = plt.get_cmap('tab20c')
 
 plt.figure(figsize=(ncols * height, nrows * height))
-for i, (tcr, g) in enumerate(pdf.groupby('tcr')):
+for i, (tcr, g) in enumerate(pdf.query('reduced_features').groupby('tcr')):
     plt.subplot(nrows, ncols, i + 1)
 
     best = 0.0
@@ -267,7 +261,7 @@ height = 2
 nrows = ntcrs // ncols + 1
 
 plt.figure(figsize=(ncols * height, nrows * height))
-groups = pdf.query('normalization == "AS" & threshold == 46.9').groupby('tcr')
+groups = pdf.query('reduced_features & normalization == "AS" & threshold == 46.9').groupby('tcr')
 for i, (tcr, g) in enumerate(groups):
     plt.subplot(nrows, ncols, i + 1)
 
@@ -323,7 +317,7 @@ def classification_metrics(g):
     })
 
 
-adf = pdf.groupby([
+adf = pdf.query('reduced_features').groupby([
     'normalization', 'threshold', 'tcr'
 ]).apply(
     classification_metrics
